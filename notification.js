eb8e6a384dac9086c6f010fb7ae591fe96d12746
Amos Banda <bandaamos98@gmail.com>
2026-06-06 22:02:32 +0200

    Update notification.js

--- notification.js
+++ notification.js
@@ -10,3 +10,11 @@ window.developerNotification = {
   "body": "we are working tirelessly to bring multiplayer mode, modes include FREE FOR ALL and TEAM UP. stay tuned",
   "icon": "📢"
 };
+
+window.developerNotification = {
+  "enabled": true,
+  "id": "update-v2-launch", 
+  "title": "Game Updates",
+  "body": "Our team is building a native apk which will soon be availbale on playstore",
+  "icon": "📢"
+};
