a972100209b1b5a243d3b3603ce96d04d7ae7aff
Amos Banda <bandaamos98@gmail.com>
2026-06-06 22:30:47 +0200

    Update notification.js

--- notification.js
+++ notification.js
@@ -1,16 +1,12 @@
-window.developerNotifications = [
-  {
-    enabled: true,
-    id: "update-v2-launch",
-    title: "Game Updates",
-    body: "We are working tirelessly to bring multiplayer mode (FREE FOR ALL and TEAM UP). Stay tuned",
-    icon: "📢"
-  },
-  {
-    enabled: true,
-    id: "apk-update",
-    title: "Game Updates",
-    body: "Our team is building a native APK which will soon be available on the Play Store",
-    icon: "📢"
-  }
-];
+/**
+ * Amos' Anagram Rush - Developer Notification Config
+ * Change 'enabled' to true to show the notification.
+ * Change the 'id' whenever you want to send a NEW notification.
+ */
+window.developerNotification = {
+  "enabled": true,
+  "id": "update-v2-launch", 
+  "title": "🏆 Competitive Season 1 is Live!",
+  "body": "We are working tirelessly to bring multiplayer mode (FREE FOR ALL and TEAM UP). Stay tuned",
+  "icon": "📢"
+};
